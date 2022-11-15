dbffa4c1c53b4cb6cd122d34c088b90c03dda76e
bpark96 <brooke.parker@selu.edu>
2022-11-15 21:18:34 +0000

    Initial commit

---
+++ R/clean.plot.R
@@ -0,0 +1,29 @@
+#' This function cleans all of the NA values from a data set, as well as creates a scatter plot using designated x and y values and saves the plot to a file.
+#' 
+#' @param filepath the name of a dataset that has been loaded into the
+#' environment or its corresponding file path 
+#' @param savefile the name that your plot file will be saved as
+#' 
+#' @return If data cleaning is unsuccessful, console will print "NAs still present!".
+#' 
+#' @examples
+#' data_cleaning(dataset_name)
+#' create_plot(dataset_name, savefile_name, x = x_value, y = y_value)
+
+data_cleaning <- function(filepath){
+  data_clean <- filepath %>%
+    drop_na()
+  if(sum(!is.na(d_c)) == 0){
+    return(data_clean)
+  } else {
+    print ("NAs still present!!")
+  }
+}
+
+create_plot <- function(filepath, savefile){
+  data_raw <- read_csv(filepath)
+  my_plot <- data_raw %>%
+    ggplot(data = data_raw, mapping = aes(x = 1, y = 1)) + geom_point()
+  ggsave(savefile, width = 20, height = 14, units = "cm", dpi = 300)
+  return(my_plot)
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+---
+title: "readme"
+author: "Brooke Parker"
+output: html_document
+date: "2022-11-15"
+---
+
+
+
+
+
+
+# data.cleaner
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of data.cleaner is to ...
+
+## Installation
+
+You can install the development version of data.cleaner like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(data.cleaner)
+## basic example code
+```
+
